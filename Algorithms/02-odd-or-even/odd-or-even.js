25ec416b8545033fe4e1e976f716c0da95b217b7
Sarah Abu Zeneh <sara.abu.zena2000@gmail.com>
2021-07-01 09:00:11 +0300

    JavaScript-Core-1-Challenges Solution

--- Algorithms/02-odd-or-even/odd-or-even.js
+++ Algorithms/02-odd-or-even/odd-or-even.js
@@ -1,4 +1,15 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
-
-var oddOrEven = function(num) {};
+function oddOrEven (num){
+    if (num%2==0)
+    {
+        return
+    }
+}
+var oddOrEven = function(num) {
+    if (num%2==0){
+        return "even";
+    }else{
+        return "odd"
+    }
+};
--- Algorithms/09-vowel-count/vowel-count.js
+++ Algorithms/09-vowel-count/vowel-count.js
@@ -1,3 +1,24 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    let vowelCounter=0;
+    for (let i = 0 ; i < str.length;i++)
+    {
+        if (str[i]=="a"){
+            vowelCounter++;
+        }
+        if (str[i]=="e"){
+            vowelCounter++;
+        }
+        if (str[i]=="i"){
+            vowelCounter++;
+        }
+        if (str[i]=="o"){
+            vowelCounter++;
+        }
+        if (str[i]=="u"){
+            vowelCounter++;
+        }  
+    }
+    console.log(vowelCounter);
+};
